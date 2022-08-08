379c8be532becdc20c80342f0417a10e69c319d4
isnow890 <53589758+isnow890@users.noreply.github.com>
2022-08-08 16:20:43 +0900

    이벤트 fire시 setState로 state의 값을 변경하기.

--- src/App.js
+++ src/App.js
@@ -12,38 +12,50 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode:'read',
+      mode: 'read',
       subject: { title: 'WEB', sub: 'World Wide Web!' },
-      welcome:{title:'Welcome',desc:'Hello,react!'},
-      contents : [
+      welcome: { title: 'Welcome', desc: 'Hello,react!' },
+      contents: [
         { id: 1, title: 'HTML', desc: 'HTML is for information' },
         { id: 2, title: 'CSS', desc: 'CSS is for design' },
         // { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
       ]
+    }
   }
-}
 
-render() {
+  render() {
 
-console.log('App render');
-let _title, _desc = null;
-if (this.state.mode === 'welcome'){
-  _title = this.state.welcome.title;
-  _desc = this.state.welcome.desc;
-}else if (this.state.mode ==='read'){
-  _title = this.state.contents[0].title;
-  _desc = this.state.contents[0].desc;
-}
+    console.log('App render');
+    let _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
 
-  return (
-    <div className="App">
-      <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-      
-      <TOC data={this.state.contents}></TOC>
-      <Content title={_title} desc={_desc}></Content>
-    </div>
-  )
-} 
+    return (
+      <div className="App">
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            // debugger;
+            e.preventDefault();
+            // alert("haha");
+            // this.state.mode="welcome";
+            
+            this.setState({mode:"welcome"});
+          }.bind(this)}> {this.state.subject.title}</a></h1>
+        </header>
+        {this.state.subject.sub}
+
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    )
+  }
 }
 // dkd
 
