8b829a9335ea102f5f562fb41d6e0d867f7f9f57
isnow890 <53589758+isnow890@users.noreply.github.com>
2022-08-08 15:40:23 +0900

    15 state

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -4,18 +4,17 @@ import React, { Component } from "react";
 
 class TOC extends Component {
     render() {
+        let lists=[];
+        let data = this.props.data;
+        let i=0;
+        while(i<data.length){
+            lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>);
+            i = i+1;
+        }
       return (
         <nav>
           <ul>
-            <li>
-              <a href="1.html">html</a>
-            </li>
-            <li>
-              <a href="2.html">css</a>
-            </li>
-            <li>
-              <a href="3.html">js</a>
-            </li>
+          {lists}
           </ul>
         </nav>
       );
