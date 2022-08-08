ec93638173ead837f794c83a23cb047b90b011d6
isnow890 <53589758+isnow890@users.noreply.github.com>
2022-08-08 15:27:56 +0900

    state의 기초

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ import reportWebVitals from './reportWebVitals';
 
 
 
+
+
+
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state={
+    subject:{title:'WEB',sub:'World Wide Web!'}
+  }
+}
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         <Subject title="React" sub="for ui"></Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
